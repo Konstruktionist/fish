0dc018466df121e29c9511b798c11fce48baef0d
Konstruktionist <pvanpeursem@gmail.com>
2015-09-02 22:23:46 +0200

    changed color of time due to new colorscheme

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,6 +1,6 @@
 function fish_right_prompt
 
-   set_color $fish_color_autosuggestion
+   set_color $fish_color_comment
    printf '[%s]' (date +%H:%M:%S)
    # prompt_git_status
    set_color normal
