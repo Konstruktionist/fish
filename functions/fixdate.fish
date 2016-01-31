4568f7b640afbb9b4c16690717c872d6fdded3fa
Konstruktionist <pvanpeursem@gmail.com>
2016-01-31 21:51:55 +0100

    Function returns a list of constructed dates

--- functions/fixdate.fish
+++ functions/fixdate.fish
@@ -4,20 +4,35 @@
 #
 
 function fixdate
-   # get list of files
-   for file in *.mp4
-   # find out if filename contains numbers
-   # in which field are these numbers, so awk knows where to look
-   # if yes, how many
-   # does it look like a date
-   # does it include a time stamp
-   # does it include a timezone specifier
-   # discard all time stamp info
-   # we assume that the date format is the following:
-   #  either yymmdd or yyyymmdd
-   ls | grep -E '[0-9]{8}'     # matches dates with eight numbers
-   ls | grep -E '[0-9]{6}'     # matches dates with six numbers
-   #  get the individual year, month and day items
-   #  plug them into the SetFile command
-end
+  #clean up from previous use
+  if test -f candidates.txt
+    rm candidates.txt
+    else
+    echo -n ''
+  end
+    if test -f errors.txt
+    rm errors.txt
+    else
+    echo -n ''
+  end
 
+  # Assumption: date is formatted as YYMMDD (6 numbers)
+  # get list of files
+  for file in *.mp4
+      # find out if filename contains numbers
+      # pattern longer than 6 numbers?
+      if test (echo $file | egrep '[0-9]{7,}')
+          # we will ignore these
+          echo $file >> errors.txt
+      else if test (echo $file | egrep '[0-9]{6}') # matches dates with six numbers
+          echo $file >> candidates.txt
+          set datum (echo $file | egrep '[0-9]{6}' | awk '{print $2}')
+          # get the individual year, month and day items
+          set year (echo $datum | cut -c 1-2)
+          set month (echo $datum | cut -c 3-4)
+          set day (echo $datum | cut -c 5-6)
+          echo date = $year-$month-$day
+          # plug them into the SetFile command
+      end
+  end
+end
