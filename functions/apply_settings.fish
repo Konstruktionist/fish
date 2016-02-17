5201815f2b847c0e004c8a45cbcc0098403f1595
Konstruktionist <pvanpeursem@gmail.com>
2016-02-17 16:46:55 +0100

    Made obvious files were created by this function

--- functions/apply_settings.fish
+++ functions/apply_settings.fish
@@ -25,8 +25,8 @@ function apply_settings -d 'setup fish universal variables'
 
 	set -U fish_color_dimmed 808080
 	set -U fish_color_separator 999
-  
-  # git colors
+
+	# git colors
 
 	set -U fish_color_git_clean 5f8700      # green
 	set -U fish_color_git_staged fae3a0     # yellow
@@ -39,7 +39,7 @@ function apply_settings -d 'setup fish universal variables'
 	set -U fish_color_git_renamed magenta
 	set -U fish_color_git_unmerged fae3a0   # yellow
 	set -U fish_color_git_untracked d14548  # red
-  set -U fish_color_git_sha 8a8a8a        # light gray
+	set -U fish_color_git_sha 8a8a8a        # light gray
 
 	set -U fish_pager_color_completion normal
 	set -U fish_pager_color_description 555 yellow
--- functions/fixdate.fish
+++ functions/fixdate.fish
@@ -9,19 +9,19 @@
 #
 # SetFile is deprecated as of Xcode 6, may not work in the future
 #
-#   version 1.3
-#   14-02-2016
+#   version 1.4
+#   17-02-2016
 #
 
 function fixdate -d 'fix file creation & modification dates'
   #clean up from previous use
-  if test -f candidates.txt
-    rm candidates.txt
+  if test -f fixdate_candidates.txt
+    rm fixdate_candidates.txt
     else
     echo -n ''
   end
-    if test -f errors.txt
-    rm errors.txt
+    if test -f fixdate_errors.txt
+    rm fixdate_errors.txt
     else
     echo -n ''
   end
@@ -33,9 +33,9 @@ function fixdate -d 'fix file creation & modification dates'
       # pattern longer than 6 digits?
       if test (echo $file | egrep '[0-9]{7,}')
           # we will ignore these
-          echo $file >> errors.txt
-      else if test (echo $file | egrep '[0-9]{6}') # match dates with six digits
-          echo $file >> candidates.txt
+          echo $file >> fixdate_errors.txt
+      else if test (echo $file | egrep '[0-9]{6}') # match dates with 6 digits
+          echo $file >> fixdate_candidates.txt
           # we don't know where the date appears, so let's grab only the
           #  matching part
           set datum (echo $file | egrep -o '[0-9]{6}')
