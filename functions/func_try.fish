e3359e0b0de016e37c00c93e70ac1be080cf1eb3
Konstruktionist <pvanpeursem@gmail.com>
2016-02-21 16:52:57 +0100

    playing with netork info

--- functions/func_try.fish
+++ functions/func_try.fish
@@ -5,54 +5,38 @@
 
 function func_try -d 'Playground function'
 
-  # get all the info we can grab from networksetup -listallhardwareports
-  #   array of friendly labels
-  set hw_port (networksetup -listallhardwareports | grep 'Hardware Port:' | awk -F : '{print $2}' | sed s/\ /""/ )
-  #   array of geeky port names
-  set device (networksetup -listallhardwareports | grep 'Device:' | awk -F : '{print $2}' | awk '{print $1}')
-  #   array of MAC adresses
-  set NWSU_MAC_Address (networksetup -listallhardwareports | grep 'Ethernet Address:' | awk '{print $3}')
-
-  # get all the info we can get from ifconfig
-  #   an array of firendly labels
-  set type (ifconfig -uv | grep 'type:' | awk -F : '{print $2}' | awk '{print $1}')
-  # 	an array of geeky port names
   set ports (ifconfig -uv | grep '^[a-z0-9]' | awk -F : '{print $1}')
-  # 	an array of MAC addresses
-  set IC_MAC_Address (ifconfig -uv | grep 'ether' | awk '{print $2}')
+  for val in $ports
+    set activated (ifconfig -uv $val | grep 'status: ' | awk '{print $2}')
+    set label (ifconfig -uv $val | grep 'type' | awk -F : '{print $2}')
+    set geekport (ifconfig -uv $val | grep '^[a-z0-9]' | awk -F : '{print $1}')
+    set state (ifconfig -uv $val | grep 'status: ' | awk '{print $2}')
+    set ipaddress (ifconfig -uv $val | grep 'inet ' | awk '{print $2}')
+    set networkspeed (ifconfig -uv $val | grep 'link rate: ' | awk '{print $3, $4}')
+    set macaddress (ifconfig -uv $val | grep 'ether ' | awk '{print $2}')
+    set netmask (ipconfig getpacket $val | grep 'subnet_mask (ip):' | awk '{print $3}')
+    set router (ipconfig getpacket $geekport | grep 'router (ip_mult):' | sed 's/.*router (ip_mult): {\([^}]*\)}.*/\1/')
+    set dnsserver (ipconfig getpacket $geekport | grep 'domain_name_server (ip_mult):' | sed 's/.*domain_name_server (ip_mult): {\([^}]*\)}.*/\1/')
+    set quality (ifconfig -uv $val | grep 'link quality:' | awk '{print $3, $4}')
+    set wifi_name (/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep '\sSSID:' | sed 's/.*: //')
 
-  echo $hw_port
-  # result [Wi-Fi, Bluetooth PAN, Thunderbolt 1, Thunderbolt 2, Thunderbolt Bridge]
-  # Total: 5 elements
-  echo $device
-  # result [en0, en3, en1, en2, bridge0]
-  # Total: 5 elements
-  echo $NWSU_MAC_Address
-  # result [ac:bc:32:b8:12:79, ac:bc:32:b8:12:7a, 4a:00:03:ea:db:f0, 4a:00:03:ea:db:f1, ae:bc:32:8b:58:00]
-  # Total: 5 elements
-  echo ''
-  echo $type
-  # result [Wi-Fi, Ethernet, Ethernet, Wi-Fi, Wi-Fi]
-  # Total: 5 elements
-  echo $ports
-  # result [lo0, en0, en1, en2, p2p0, awdl0, bridge0, utun0]
-  # Total: 8 elements
-  echo $IC_MAC_Address
-  # result [ac:bc:32:b8:12:79, 4a:00:03:ea:db:f0, 4a:00:03:ea:db:f1, 0e:bc:32:b8:12:79, 7e:1c:4b:9b:cb:82, ae:bc:32:8b:58:00]
-  # Total: 6 elements
-
-  #   We want to use the friendly labels from networksetup but the data from ifconfig
-  #   The only thing that matches in both is the MAC address
-
-  echo '------------------------------------------------'
-  for i in  $IC_MAC_Address
-    for j in $NWSU_MAC_Address
-    # set counter (seq (count [$j]))
-      if test $i = $j
-        set_color green; echo -n $i; echo -n ' =? '; echo $j; set_color normal
-      else
-        set_color red; echo -n $i; echo -n ' =? '; echo $j; set_color normal
+    if test $activated = 'active' ^/dev/null
+      if test -n "$ipaddress"
+        echo -n $label ; echo -n ' ('; echo -n $geekport ; echo ')'
+        echo "--------------"
+        if test -n "$wifi_name"
+          echo -n ' Network Name: ' ; echo $wifi_name
+        end
+        echo -n '   IP-address: ' ; echo $ipaddress
+        echo -n '  Subnet Mask: ' ; echo $netmask
+        echo -n '       Router: ' ; echo $router
+        echo -n '   DNS Server: ' ; echo $dnsserver
+        echo -n '  MAC-address: ' ; echo $macaddress
+        echo -n 'Network Speed: ' ; echo $networkspeed
+        echo -n 'Netw. quality: ' ; echo $quality
+        echo " "
       end
     end
   end
+
 end
