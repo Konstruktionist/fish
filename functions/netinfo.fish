2d882fbc317f22e938b0fd7db3540a4e2b26d361
Konstruktionist <pvanpeursem@gmail.com>
2016-02-21 20:50:37 +0100

    Removed redundant geekport line

--- functions/netinfo.fish
+++ functions/netinfo.fish
@@ -3,20 +3,21 @@
 # PROBLEM: getnet.fish has en0 & en1 hardcoded to Ethernet & Wi-Fi.
 #   This works if there is a physical ethernet port on the computer
 #   but current MacBooks may not have one.
-#   This also displays all network interfaces, not just Ethernet & Wi-Fi.
 #
 # SOLUTION: find out what ports are available on the machine & how
 #   they map to Ethernet and Wi-Fi. Then use this information to
 #   display the correct port with the user friendly labels.
 #
-#   version 1.1
+#   This also should display all network interfaces, not just Ethernet & Wi-Fi.
+#
+#   version 1.2
 #   21-02-2016
 
 function netinfo -d "get network information"
 
   set public (curl -s http://ipecho.net/plain)
-  if test -z "$public" # No internet connection
-    set public "No internet connection available"
+  if test -z "$public" # No Internet connection
+    set public "No Internet connection available"
   end
 
   echo " "
@@ -24,28 +25,29 @@ function netinfo -d "get network information"
   echo "     Hostname:" (uname -n)
   echo " "
 
+  # Get all available hardware ports
   set ports (ifconfig -uv | grep '^[a-z0-9]' | awk -F : '{print $1}')
+  # Get WiFi network name
   set wifi_name (/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep '\sSSID:' | sed 's/.*: //')
 
-  for val in $ports
+  for val in $ports   # Get for all available hardware ports their status
     set activated (ifconfig -uv $val | grep 'status: ' | awk '{print $2}')
     set label (ifconfig -uv $val | grep 'type' | awk '{print $2}')
-    set geekport (ifconfig -uv $val | grep '^[a-z0-9]' | awk -F : '{print $1}')
     set state (ifconfig -uv $val | grep 'status: ' | awk '{print $2}')
     set ipaddress (ifconfig -uv $val | grep 'inet ' | awk '{print $2}')
     set networkspeed (ifconfig -uv $val | grep 'link rate: ' | awk '{print $3, $4}')
     set macaddress (ifconfig -uv $val | grep 'ether ' | awk '{print $2}')
     set quality (ifconfig -uv $val | grep 'link quality:' | awk '{print $3, $4}')
     set netmask (ipconfig getpacket $val | grep 'subnet_mask (ip):' | awk '{print $3}')
-    set router (ipconfig getpacket $geekport | grep 'router (ip_mult):' | sed 's/.*router (ip_mult): {\([^}]*\)}.*/\1/')
-    set dnsserver (ipconfig getpacket $geekport | grep 'domain_name_server (ip_mult):' | sed 's/.*domain_name_server (ip_mult): {\([^}]*\)}.*/\1/')
+    set router (ipconfig getpacket $val | grep 'router (ip_mult):' | sed 's/.*router (ip_mult): {\([^}]*\)}.*/\1/')
+    set dnsserver (ipconfig getpacket $val | grep 'domain_name_server (ip_mult):' | sed 's/.*domain_name_server (ip_mult): {\([^}]*\)}.*/\1/')
 
     # We want information about active network ports...
     if test $activated = 'active' ^/dev/null
-    #   and of these, the ones with an ip-address assigned to it
-      if test -n "$ipaddress" # Do we have an ip address?
+    #   and of these, the ones with an IP-address assigned to it
+      if test -n "$ipaddress" # Do we have an IP address?
       # Give us the information
-        echo -n $label ; echo -n ' ('; echo -n $geekport ; echo ')'
+        echo -n $label ; echo -n ' ('; echo -n $val ; echo ')'
         echo "--------------"
         # Is this a WiFi associated port? If so, then we want the network name
         switch $label
