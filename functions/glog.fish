a7f35ce9678c09b8a876e14917616787155e054a
Konstruktionist <pvanpeursem@gmail.com>
2016-02-25 13:38:16 +0100

    Changed format of pretty log output hash - commit message (relative time) <committer>

--- functions/glog.fish
+++ functions/glog.fish
@@ -1,3 +1,3 @@
 function glog -d 'A nice git log display'
-  git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative
+  git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --
 end
