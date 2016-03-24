cdef774c1eb578db6d051ed55eb9012064504043
Konstruktionist <pvanpeursem@gmail.com>
2016-03-24 18:59:02 +0100

    Removed trailing whitepace

--- functions/mdb.fish
+++ functions/mdb.fish
@@ -5,11 +5,11 @@
 #
 #   version 1.0
 #   28-02-2016
- 
+
 function mdb -d 'Make a flatfile movie database'
 
   set drive (pwd | awk -F / '{print $3}')
-  
+
   # clean up from previous use
   if test -f $HOME/Movies\ on\ $drive.txt
     rm $HOME/Movies\ on\ $drive.txt
@@ -22,9 +22,7 @@ function mdb -d 'Make a flatfile movie database'
     set width (mdls $file | grep kMDItemPixelWidth | awk '{print $3}')
     set height (mdls $file | grep kMDItemPixelHeight | awk '{print $3}')
     set tags (tag -lN $file)
-    
+
     echo $title '|' $width 'x' $height '|' $tags >> "$HOME/Movies on $drive.txt"
    end
-
-  
 end
