d6241b9ffc1aeb76fb8f98e335170955bb20c9d2
Konstruktionist <pvanpeursem@gmail.com>
2016-04-08 10:21:39 +0200

    preserve the current working directory

--- functions/updhosts.fish
+++ functions/updhosts.fish
@@ -1,7 +1,9 @@
 function updhosts -d 'Update my /etc/hosts file'
+   set current_working_directory $PWD
    cd ~/workspace
    curl -s http://someonewhocares.org/hosts/zero/hosts > my_current_hosts
    cat my_blocklist >> my_current_hosts
    sudo mv my_current_hosts /etc/hosts
+   cd $current_working_directory
 end
 
