4aacf7bfb0bd701c47cf56deba2ab0adb0d574c6
Konstruktionist <pvanpeursem@gmail.com>
2016-05-06 15:44:59 +0200

    Added fish_color_terminator setting

--- functions/apply_settings.fish
+++ functions/apply_settings.fish
@@ -22,6 +22,7 @@ function apply_settings -d 'setup fish universal variables'
 	set -U fish_color_status b91e2e
 	set -U fish_color_user 5f875f
 	set -U fish_color_valid_path --underline
+  set -U fish_color_terminator f3b26d   # orange
 
 	set -U fish_color_dimmed 808080
 	set -U fish_color_separator 999
