3730f9025deb96339329f5cb1608c556fe9941d9
Konstruktionist <pvanpeursem@gmail.com>
2016-05-27 00:42:47 +0200

    Added lmas: List installed MacAppStore apps

---
+++ functions/lmas.fish
@@ -0,0 +1,3 @@
+function lmas -d "List all Mac App Store downloads"
+  find /Applications -path '*Contents/_MASReceipt/receipt' -maxdepth 4 -print |\sed 's#.app/Contents/_MASReceipt/receipt#.app#g; s#/Applications/##'
+end
