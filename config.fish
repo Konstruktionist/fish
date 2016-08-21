36bc4041e94322849cf9fa65a09fca06466c1be4
Konstruktionist <pvanpeursem@gmail.com>
2016-08-21 17:22:27 +0200

    Disable Google analytics for homebrew

--- config.fish
+++ config.fish
@@ -52,3 +52,6 @@ setenv -gx GREP_OPTIONS "--color=auto"
 setenv -gx LSCOLORS 'gxfxbdaeBxxehehbaghbad'          # see man ls
 
 set -gx SOCCER_CLI_API_TOKEN "f8e416cdeeb24e33824e96b1bcfc3961"
+
+# disable Google analytics for homebrew
+set -gx HOMEBREW_NO_ANALYTICS 1
