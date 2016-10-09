0abf68948d104e9e86fb583babee4d9670bd69e1
Konstruktionist <pvanpeursem@gmail.com>
2016-10-09 14:37:04 +0200

    Add a \n to last line as workaround for fish issue #789

--- functions/xpstatus.fish
+++ functions/xpstatus.fish
@@ -5,6 +5,6 @@ function xpstatus -d "Check version of XProtect"
 
   set timestamp (GetFileInfo /System/Library/CoreServices/XProtect.bundle/Contents/Resources/XProtect.meta.plist | grep 'modified' | awk '{print $3}')
   
-  echo -n "Xprotect is at version $version, updated on $datum at $timestamp."
+  echo -n "Xprotect is at version $version, updated on $datum at $timestamp."\n
 
 end
