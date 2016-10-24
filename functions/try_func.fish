667c24be8538ac77ceec3efd3e5e3c934489889f
Konstruktionist <pvanpeursem@gmail.com>
2016-10-24 22:39:09 +0200

    Trying out a new chost function

--- functions/try_func.fish
+++ functions/try_func.fish
@@ -3,40 +3,78 @@
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
 
-function func_try -d 'Playground function'
+function try_func
 
-  set ports (ifconfig -uv | grep '^[a-z0-9]' | awk -F : '{print $1}')
-  for val in $ports
-    set activated (ifconfig -uv $val | grep 'status: ' | awk '{print $2}')
-    set label (ifconfig -uv $val | grep 'type' | awk -F : '{print $2}')
-    set geekport (ifconfig -uv $val | grep '^[a-z0-9]' | awk -F : '{print $1}')
-    set state (ifconfig -uv $val | grep 'status: ' | awk '{print $2}')
-    set ipaddress (ifconfig -uv $val | grep 'inet ' | awk '{print $2}')
-    set networkspeed (ifconfig -uv $val | grep 'link rate: ' | awk '{print $3, $4}')
-    set macaddress (ifconfig -uv $val | grep 'ether ' | awk '{print $2}')
-    set netmask (ipconfig getpacket $val | grep 'subnet_mask (ip):' | awk '{print $3}')
-    set router (ipconfig getpacket $geekport | grep 'router (ip_mult):' | sed 's/.*router (ip_mult): {\([^}]*\)}.*/\1/')
-    set dnsserver (ipconfig getpacket $geekport | grep 'domain_name_server (ip_mult):' | sed 's/.*domain_name_server (ip_mult): {\([^}]*\)}.*/\1/')
-    set quality (ifconfig -uv $val | grep 'link quality:' | awk '{print $3, $4}')
-    set wifi_name (/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep '\sSSID:' | sed 's/.*: //')
+  set normal (set_color normal)
 
-    if test $activated = 'active' ^/dev/null
-      if test -n "$ipaddress"
-        echo -n $label ; echo -n ' ('; echo -n $geekport ; echo ')'
-        echo "--------------"
-        if test -n "$wifi_name"
-          echo -n ' Network Name: ' ; echo $wifi_name
-        end
-        echo -n '   IP-address: ' ; echo $ipaddress
-        echo -n '  Subnet Mask: ' ; echo $netmask
-        echo -n '       Router: ' ; echo $router
-        echo -n '   DNS Server: ' ; echo $dnsserver
-        echo -n '  MAC-address: ' ; echo $macaddress
-        echo -n 'Network Speed: ' ; echo $networkspeed
-        echo -n 'Netw. quality: ' ; echo $quality
-        echo " "
-      end
+  set -l my_hosts (cat /etc/hosts | egrep -i 'last updated' | awk '{print $5, $6, $7}')
+
+  set -l my_date (strptime -i "%d %b %Y" $my_hosts)
+
+  echo -s 'My current hosts file was updated on: ' $my_hosts
+
+
+  # Check Someonewhocares.org
+  set -l swc ( curl -s http://someonewhocares.org/hosts/zero/hosts | egrep -i 'last updated' | awk '{print $5, $6, $7}')
+  set -l distant_date (strptime -i "%d %b %Y" $swc)
+  set -l difference (datediff $my_date $distant_date)
+    if test $difference -le 0
+    set abs ( math $difference\*-1)
+      set display_color (set_color yellow)
+      set ah_beh "behind"
+    else
+      set display_color (set_color red)
+      set abs $difference
+      set ah_beh "ahead"
     end
-  end
+  echo -ns "  someonewhocares updated on: " $swc " which is " $display_color $abs $normal " day(s) " $ah_beh"."\n
+
+
+  # Check yoyo
+  set -l yoyo ( curl -s 'https://pgl.yoyo.org/as/serverlist.php?hostformat=hosts&showintro=0&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext' | egrep -i 'last updated' | awk '{print $5, $6, $7}')
+  set -l distant_date (strptime -i "%d %b %Y" $yoyo)
+  set -l difference (datediff $my_date $distant_date)
+    if test $difference -le 0
+    set abs ( math $difference\*-1)
+      set display_color (set_color yellow)
+      set ah_beh "behind"
+    else
+      set display_color (set_color red)
+      set abs $difference
+      set ah_beh "ahead"
+    end
+  echo -ns "             yoyo updated on: " $yoyo " which is " $display_color $abs $normal " day(s) " $ah_beh"."\n
+
+
+  # Check StevenBlack
+  set -l StevenBlack ( curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-social/hosts | egrep 'Date:' | awk '{print $4, $3, $5}')
+  set -l distant_date (strptime -i "%d %b %Y" $StevenBlack)
+  set -l difference (datediff $my_date $distant_date)
+    if test $difference -le 0
+    set abs ( math $difference\*-1)
+      set display_color (set_color yellow)
+      set ah_beh "behind"
+    else
+      set display_color (set_color red)
+      set abs $difference
+      set ah_beh "ahead"
+    end
+  echo -ns "      StevenBlack updated on: " $StevenBlack " which is " $display_color $abs $normal " day(s) " $ah_beh"."\n
+
+
+  # Check MVPS
+  set -l mvps (curl -s http://winhelp2002.mvps.org/hosts.txt | egrep -i 'Updated' | sed 's/^#[[:space:]]-*[[:space:]][A-z]*:[[:space:]]//' | awk '{print $1}' | awk -F - '{print $2, $1, $3}')
+  set -l distant_date (strptime -i "%d %B %Y" $mvps)
+  set -l difference (datediff $my_date $distant_date)
+    if test $difference -le 0
+    set abs ( math $difference\*-1)
+      set display_color (set_color yellow)
+      set ah_beh "behind"
+    else
+      set display_color (set_color red)
+      set abs $difference
+      set ah_beh "ahead"
+    end
+  echo -ns "             mvps updated on: " $mvps " which is " $display_color $abs $normal " day(s) " $ah_beh"."\n
 
 end
