12502048d083d48861bd00a614b72bf3b554660a
Konstruktionist <pvanpeursem@gmail.com>
2016-10-27 12:42:27 +0200

    I'm an idiot! dates are not names
    Why:
    
    * It all works when the dates are different. But when the dates of the
      different hosts files are the same, the ONLY name that gets matched in
      the switch block is the first one (i.e. someonewhocares). Because I'm
      comparing dates, not the hosts names.
      No fix yet, I have to think about this for a while.
    
    This change addresses the need by:
    
    * Me

--- functions/try_func.fish
+++ functions/try_func.fish
@@ -5,34 +5,48 @@
 
 function try_func
 
+  # Re-usable colors
   set normal (set_color normal)
+  set decor (set_color 87afff) # blue
 
+  # Get the dates from the hosts files & put them in a uniform format.
+  # format is "dd Mon Year" where dd is the day of the month in numbers, Mon is
+  # the 3-letter abreviation of the month name and Year is the year
+  # including the century.
   set -l my_hosts (cat /etc/hosts | egrep -i 'last updated' | awk '{print $5, $6, $7}')
-
-  set -l my_date (strptime -i "%d %b %Y" $my_hosts)
-
   set -l swc ( curl -s http://someonewhocares.org/hosts/zero/hosts | egrep -i 'last updated' | awk '{print $5, $6, $7}')
   set -l yoyo ( curl -s 'https://pgl.yoyo.org/as/serverlist.php?hostformat=hosts&showintro=0&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext' | egrep -i 'last updated' | awk '{print $5, $6, $7}')
-  set -l sb ( curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-social/hosts | egrep 'Date:' | awk '{print $4, substr($3,1,3), $5}')
+  set -l sb ( curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling/hosts | egrep 'Date:' | awk '{print $4, substr($3,1,3), $5}')
   set -l mvps (curl -s http://winhelp2002.mvps.org/hosts.txt | egrep -i 'Updated' | sed 's/^#[[:space:]]-*[[:space:]][A-z]*:[[:space:]]//' | awk '{print $1}' | awk -F - '{print $2, substr($1,1,3), $3}')
 
-  set decor (set_color green)
+  # The date is just a string, here we convert it to a format that dateutils can
+  # work with.
+  set -l my_date (strptime -i "%d %b %Y" $my_hosts)
+
+
   echo -s ' My hosts file was updated on: ' $decor $my_hosts $normal
 
   for val in $swc $yoyo $sb $mvps
-    switch $val;
-      case $swc;
-        set name 'someonewhocares';
-      case $yoyo;
-        set name '           yoyo';
-      case $sb;
-        set name '    StevenBlack';
-      case $mvps;
-        set name '           mvps';
+    switch $val
+      case $swc
+        set name 'someonewhocares'
+      case $yoyo
+        set name '           yoyo'
+      case $sb
+        set name '    stevenblack'
+      case $mvps
+        set name '           mvps'
     end
+    # Convert remote date strings for use with dateutils
       set -l distant_date (strptime -i "%d %b %Y" $val)
+      # Calculate the difference in days between local and remote hosts files
       set -l difference (datediff $my_date $distant_date)
+      # Figure out if remote is ahead (positive difference) or behind (negative
+      # difference) and adjust color and message to it.
         if test $difference -le 0
+        # We don't want to show negative numbers. (It took some time to figure
+        # out that we need to escape the * with a backslash. Without it we get
+        # a wildcard error message.)
         set abs ( math $difference\*-1)
           set display_color (set_color yellow)
           set ah_beh "behind"
@@ -41,7 +55,6 @@ function try_func
           set abs $difference
           set ah_beh "ahead"
       end
-  echo -ns "   " $name " updated on: " $val " which is " $display_color $abs $normal " day(s) " $ah_beh"."\n
-  # echo -ns "  on " $val ", which is " $display_color $abs $normal " day(s) " $ah_beh " was " $decor $name $normal " updated."\n
+    echo -ns "   " $name " updated on: " $val " which is " $display_color $abs $normal " day(s) " $ah_beh"."\n
   end
 end
