9eefafd436528adcb73e602fafa6bfedf2a48031
Konstruktionist <pvanpeursem@gmail.com>
2016-10-27 14:33:43 +0200

    Fixed the switching on names issue
    Why:
    
    * It all works when the dates are different. But when the dates of the
      different hosts files are the same, the ONLY name that gets matched in
      the switch block is the first one (i.e. someonewhocares). Because I'm
      comparing dates, not the hosts names.
    
      By naming the variable dates after the sites, I was led to believe
      that the dates and the sites were the same thing.
      Now I named the date variables explicitly as date and added variables
      for the names of the site.
      Now we switch on names and fill in the values accordingly.
    
      Lesson learned: naming variables to what they represent is important!
    
    This change addresses the need by:
    
    * Me

--- functions/chosts.fish
+++ functions/chosts.fish
@@ -6,8 +6,8 @@
 #     project homepage: http://www.fresse.org/dateutils/
 #     installed via homebrew
 #
-#     version 1.1
-#     25-10-2016
+#     version 1.2
+#     27-10-2016
 
 function chosts -d 'check online hosts files for last update'
 
@@ -19,32 +19,43 @@ function chosts -d 'check online hosts files for last update'
   # format is "dd Mon Year" where dd is the day of the month in numbers, Mon is
   # the 3-letter abreviation of the month name and Year is the year
   # including the century.
-  set -l my_hosts (cat /etc/hosts | egrep -i 'last updated' | awk '{print $5, $6, $7}')
-  set -l swc ( curl -s http://someonewhocares.org/hosts/zero/hosts | egrep -i 'last updated' | awk '{print $5, $6, $7}')
-  set -l yoyo ( curl -s 'https://pgl.yoyo.org/as/serverlist.php?hostformat=hosts&showintro=0&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext' | egrep -i 'last updated' | awk '{print $5, $6, $7}')
-  set -l sb ( curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling/hosts | egrep 'Date:' | awk '{print $4, substr($3,1,3), $5}')
-  set -l mvps (curl -s http://winhelp2002.mvps.org/hosts.txt | egrep -i 'Updated' | sed 's/^#[[:space:]]-*[[:space:]][A-z]*:[[:space:]]//' | awk '{print $1}' | awk -F - '{print $2, substr($1,1,3), $3}')
+  set -l my_hosts_date (cat /etc/hosts | egrep -i 'last updated' | awk '{print $5, $6, $7}')
+  set -l swc_date ( curl -s http://someonewhocares.org/hosts/zero/hosts | egrep -i 'last updated' | awk '{print $5, $6, $7}')
+  set -l yoyo_date ( curl -s 'https://pgl.yoyo.org/as/serverlist.php?hostformat=hosts&showintro=0&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext' | egrep -i 'last updated' | awk '{print $5, $6, $7}')
+  set -l sb_date ( curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling/hosts | egrep 'Date:' | awk '{print $4, substr($3,1,3), $5}')
+  set -l mvps_date (curl -s http://winhelp2002.mvps.org/hosts.txt | egrep -i 'Updated' | sed 's/^#[[:space:]]-*[[:space:]][A-z]*:[[:space:]]//' | awk '{print $1}' | awk -F - '{print $2, substr($1,1,3), $3}')
+
+  # These are used later to fill in the right values
+  set -l swc 'swc'
+  set -l yoyo 'yoyo'
+  set -l sb 'sb'
+  set -l mvps 'mvps'
 
   # The date is just a string, here we convert it to a format that dateutils can
   # work with.
-  set -l my_date (strptime -i "%d %b %Y" $my_hosts)
+  set -l my_date (strptime -i "%d %b %Y" $my_hosts_date)
 
 
-  echo -s ' My hosts file was updated on: ' $decor $my_hosts $normal
+  echo -s ' My hosts file was updated on: ' $decor $my_hosts_date $normal
 
   for val in $swc $yoyo $sb $mvps
     switch $val
-      case $swc
+      case swc
         set name 'someonewhocares'
-      case $yoyo
+        set date $swc_date
+      case yoyo
         set name '           yoyo'
-      case $sb
+        set date $yoyo_date
+      case sb
         set name '    stevenblack'
-      case $mvps
+        set date $sb_date
+      case mvps
         set name '           mvps'
+        set date $mvps_date
     end
+
     # Convert remote date strings for use with dateutils
-      set -l distant_date (strptime -i "%d %b %Y" $val)
+      set -l distant_date (strptime -i "%d %b %Y" $date)
       # Calculate the difference in days between local and remote hosts files
       set -l difference (datediff $my_date $distant_date)
       # Figure out if remote is ahead (positive difference) or behind (negative
@@ -64,3 +75,4 @@ function chosts -d 'check online hosts files for last update'
     echo -ns "   " $name " updated on: " $val " which is " $display_color $abs $normal " day(s) " $ah_beh"."\n
   end
 end
+
