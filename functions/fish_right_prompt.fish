208e8de2f0f446fe6e76852fd719350d53fad4db
Konstruktionist <pvanpeursem@gmail.com>
2016-11-05 20:33:16 +0100

    Added command-execution-time functionality
    Why:
    
    * see commit 464ac2f6188e3119ab99bdf9f00bcf851f923366
    
    This change addresses the need by:
    
    * Me

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,7 +1,8 @@
 function fish_right_prompt
 
-  set_color $fish_color_autosuggestion
-  printf '[%s]' (date +%H:%M:%S)
+  set_color 808080 # grey
+  # execution time last command & time of day
+  echo -ns " $CMD_DURATION_STR | " (date +%H:%M:%S)
   # prompt_git_status
   set_color normal
 end
