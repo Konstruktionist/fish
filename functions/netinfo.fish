b822058e2444a3108a51488b178da9579b0d35d9
Konstruktionist <pvanpeursem@gmail.com>
2016-11-07 16:52:04 +0100

    Ask for info when we need it. Now 4x faster
    Why:
    
    * We asked all the info beforehand, even from inactive ports.
      This is a waste of time. By asking for the info at the last moment
      when we actually need it, we save a lot of time.
    
      Timing went from 1072 ms to 261 ms.
    
    This change addresses the need by:
    
    * Me

--- functions/netinfo.fish
+++ functions/netinfo.fish
@@ -10,56 +10,63 @@
 #
 #   This also should display all network interfaces, not just Ethernet & Wi-Fi.
 #
-#   version 1.5
-#   26-09-2016
+#   version 1.6
+#   07-11-2016
 
 function netinfo -d "get network information"
 
   # Get public ip address
   set public (curl -s http://ipecho.net/plain)
   # Let's account for possible lag in reply from ipecho.net
-  sleep .3
+  # sleep .3
 
   if test -z "$public" # No Internet connection
     set public "No Internet connection available"
   end
 
   echo " "
-  echo "    Public IP: $public"
+  echo "    Public IP:" $public
   echo "     Hostname:" (uname -n)
   echo " "
 
   # Get all available hardware ports
   set ports (ifconfig -uv | grep '^[a-z0-9]' | awk -F : '{print $1}')
-  # Get WiFi network name
-  set wifi_name (/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep '\sSSID:' | sed 's/.*: //')
 
-  for val in $ports   # Get for all available hardware ports their status
+  # Get for all available hardware ports their status
+  for val in $ports
     set activated (ifconfig -uv $val | grep 'status: ' | awk '{print $2}')
-    set label (ifconfig -uv $val | grep 'type' | awk '{print $2}')
-    set state (ifconfig -uv $val | grep 'status: ' | awk '{print $2}')
-    set ipaddress (ifconfig -uv $val | grep 'inet ' | awk '{print $2}')
-    set networkspeed (ifconfig -uv $val | grep 'link rate:' | awk '{print $3, $4}')
-    set macaddress (ifconfig -uv $val | grep 'ether ' | awk '{print $2}')
-    set quality (ifconfig -uv $val | grep 'link quality:' | awk '{print $3, $4}')
-    set netmask (ipconfig getpacket $val | grep 'subnet_mask (ip):' | awk '{print $3}')
-    set router (ipconfig getpacket $val | grep 'router (ip_mult):' | sed 's/.*router (ip_mult): {\([^}]*\)}.*/\1/')
-    set dnsserver (networksetup -getdnsservers $label | awk '{print $1, $2}')
 
     # We want information about active network ports...
     if test $activated = 'active' ^/dev/null
+      set ipaddress (ifconfig -uv $val | grep 'inet ' | awk '{print $2}')
+
       # and of these, the ones with an IP-address assigned to it
-      if test -n "$ipaddress" # Do we have an IP address?
-        # Give us the information
+      if test -n "$ipaddress" ^/dev/null
+
+        # Do we have an IP address?
+        # Then give us the information
+        set label (ifconfig -uv $val | grep 'type' | awk '{print $2}')
+        set macaddress (ifconfig -uv $val | grep 'ether ' | awk '{print $2}')
+        set quality (ifconfig -uv $val | grep 'link quality:' | awk '{print $3, $4}')
+        set netmask (ipconfig getpacket $val | grep 'subnet_mask (ip):' | awk '{print $3}')
+        set router (ipconfig getpacket $val | grep 'router (ip_mult):' | sed 's/.*router (ip_mult): {\([^}]*\)}.*/\1/')
+        set dnsserver (networksetup -getdnsservers $label | awk '{print $1, $2}')
+
+        # Header for the network interfaces
         echo -n $label ; echo -n ' ('; echo -n $val ; echo ')'
         echo "--------------"
+
         # Is this a WiFi associated port? If so, then we want the network name
         switch $label
           case Wi-Fi
-            echo -n  ' Network Name: '; echo $wifi_name
-            # Networkspeed for Wi-Fi is reported differently (more info) then
-            # ethernet so we adjust for that here
-            set networkspeed (ifconfig -uv $val | grep 'downlink rate:' | awk '{print $7, $8}')
+            # Get WiFi network name
+            set wifi_name (/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep '\sSSID:' | sed 's/.*: //')
+            echo -n ' Network Name: '; echo $wifi_name
+            # Networkspeed for Wi-Fi
+            set networkspeed (/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep lastTxRate: | sed 's/.*: //' | sed 's/$/ Mbps/')
+          case '*'
+            # Networkspeed  for other ports
+            set networkspeed (ifconfig -uv $val | grep 'link rate:' | awk '{print $3, $4}')
         end
 
         echo -n '   IP-address: ' ; echo $ipaddress
