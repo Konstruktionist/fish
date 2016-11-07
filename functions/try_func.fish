aa38be4b4301b2c254dd9a22d9a77b0e908e2ec5
Konstruktionist <pvanpeursem@gmail.com>
2016-11-07 16:47:46 +0100

    More tweaking of function speed
    Why:
    
    * By asking the info when we need it, and not beforehand we get even
      more speed increase
    
    This change addresses the need by:
    
    * Me

--- functions/try_func.fish
+++ functions/try_func.fish
@@ -15,7 +15,7 @@ function try_func
   end
 
   echo " "
-  echo "    Public IP: $public"
+  echo "    Public IP:" $public
   echo "     Hostname:" (uname -n)
   echo " "
 
@@ -36,13 +36,13 @@ function try_func
         # Do we have an IP address?
         # Then give us the information
         set label (ifconfig -uv $val | grep 'type' | awk '{print $2}')
-        set networkspeed (ifconfig -uv $val | grep 'link rate:' | awk '{print $3, $4}')
         set macaddress (ifconfig -uv $val | grep 'ether ' | awk '{print $2}')
         set quality (ifconfig -uv $val | grep 'link quality:' | awk '{print $3, $4}')
         set netmask (ipconfig getpacket $val | grep 'subnet_mask (ip):' | awk '{print $3}')
         set router (ipconfig getpacket $val | grep 'router (ip_mult):' | sed 's/.*router (ip_mult): {\([^}]*\)}.*/\1/')
         set dnsserver (networksetup -getdnsservers $label | awk '{print $1, $2}')
 
+        # Header for the network interfaces
         echo -n $label ; echo -n ' ('; echo -n $val ; echo ')'
         echo "--------------"
 
@@ -51,9 +51,12 @@ function try_func
           case Wi-Fi
             # Get WiFi network name
             set wifi_name (/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep '\sSSID:' | sed 's/.*: //')
-            echo -n  ' Network Name: '; echo $wifi_name
+            echo -n ' Network Name: '; echo $wifi_name
             # Networkspeed for Wi-Fi
             set networkspeed (/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep lastTxRate: | sed 's/.*: //' | sed 's/$/ Mbps/')
+          case '*'
+            # Networkspeed  for other ports
+            set networkspeed (ifconfig -uv $val | grep 'link rate:' | awk '{print $3, $4}')
         end
 
         echo -n '   IP-address: ' ; echo $ipaddress
