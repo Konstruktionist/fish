92c5bd90b954497ade71798572f00f8639ca2817
Konstruktionist <pvanpeursem@gmail.com>
2016-11-08 15:52:19 +0100

    Removed the git log (glog) function
    Why:
    
    * glog output was messy. I'm now using the method that was shown in
      DAS #63. Specifically this gist:
      https://gist.github.com/skanev/2521741
    
      Now we have color and nice aligning columns
    
    This change addresses the need by:
    
    * Me

--- functions/glog.fish
+++
@@ -1,3 +0,0 @@
-function glog -d 'A nice git log display'
-  git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --
-end
