4f982d81efc9da036b5f739d672fb5e582176fce
Konstruktionist <pvanpeursem@gmail.com>
2016-11-09 23:30:12 +0100

    Display more info, don't wipe screen after exit
    Why:
    
    * 3 lines of info proved to be too little, now its 7 lines surrounding
       the search term. Also before after viewing the manpage the screen
       was blanked, which isn't helpful if you're trying out options or
       trying to understand the sometimes terse explanations. Now we have
       the information still on screen and we can refer to it.
    
    This change addresses the need by:
    
    * Me

--- functions/mans.fish
+++ functions/mans.fish
@@ -1,8 +1,10 @@
-#   mans:   Search manpage given in agument '1' for term given in argument '2' (case insensitive)
-#           displays paginated result with colored search terms and three lines surrounding each hit.
+#   mans:   Search manpage given in agument '1' for term given in argument '2'
+#            (case insensitive)
+#           displays paginated result with colored search terms and seven lines
+#           surrounding each hit.
 #             Example: mans mplayer codec
-#   --------------------------------------------------------------------
+#   ---------------------------------------------------------------------------
 
 function mans -d 'Search manpage given in argument 1 for term given in argument 2'
-  man $argv[1] | grep -iC3 --color=always $argv[2] | less -R
+  man $argv[1] | grep -iC7 --color=always $argv[2] | less -RX
 end
