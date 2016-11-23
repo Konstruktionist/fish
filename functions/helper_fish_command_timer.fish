b009a0010ed7e7f7df63e4a437aeff68fa4aa966
Konstruktionist <pvanpeursem@gmail.com>
2016-11-23 13:36:48 +0100

    Fixed url
    Why:
    
    * The url in the source code was a dead link on github.
    
    This change addresses the need by:
    
    * Me

--- functions/helper_fish_command_timer.fish
+++ functions/helper_fish_command_timer.fish
@@ -20,7 +20,7 @@
 #
 # For the most up-to-date version, as well as further information and
 # installation instructions, please visit the GitHub project page at
-#     https://github.com/jichuan89/fish-command-timer
+#     https://github.com/jichu4n/fish-command-timer
 #
 # Requires fish 2.2 or above.
 
