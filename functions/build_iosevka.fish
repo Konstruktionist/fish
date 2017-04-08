1802f4ede03f80f6a127022bef005531326f7a98
Konstruktionist <pvanpeursem@gmail.com>
2017-04-08 17:39:25 +0200

    Build iosevka from anywhere
    Why:
    
    * I don't have to be in the Iosevka-master directory to build the font.
      Fine-tuned the build with new options
    
    This change addresses the need by:
    
    * Me

--- functions/build_iosevka.fish
+++ functions/build_iosevka.fish
@@ -1,4 +1,8 @@
 function build_iosevka -d "build custom version Iosevka font"
+
+  # Save current working directory, so we can excecute this anywhere in the file system.
+  set current_working_directory $PWD
+
   cd ~/Downloads/Iosevka-master
 
   # Upgrade to latest version of required tools
@@ -8,10 +12,13 @@ function build_iosevka -d "build custom version Iosevka font"
   # Install required libraries for nodejs
   npm install
   # The custom configuration
-  make custom-config design='v-asterisk-low'
+  make custom-config design='v-asterisk-low v-underscore-low v-brace-straight v-numbersign-slanted'
   # Build font
   make custom
   cd dist/iosevka-custom
   # We have italics in gui & terminal, so we don't need obliques
   rm *oblique*
+
+  # Return to where we came from
+  cd $current_working_directory
 end
