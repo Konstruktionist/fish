3b96de66e07d65d2f21f0f1fd3972b3ac647b24d
Konstruktionist <pvanpeursem@gmail.com>
2017-04-08 17:21:25 +0200

    Optimizations to improve startuptime
    Why:
    
    * After reading on the Gitter channel, I profiled the startuptime and
      looked where improvements could be made. Noticed that sentenv spawned
      an internal fish function that translated setenv to set $argv. So I do
      this myself now.
    
      Also moved the abbreviations to functions.
      Removed HOMEBREW_NO_ANALYTICS, we can do this from within brew itself
    
    This change addresses the need by:
    
    * Me

--- config.fish
+++ config.fish
@@ -12,8 +12,8 @@ set -gx ARCHFLAGS "-arch x86_64"
 # Set the editor
 set -gx EDITOR mate
 # Let homebrew know about my preferred editor
-setenv -gx HOMEBREW_EDITOR mate
-setenv -gx VISUAL mate
+set -gx HOMEBREW_EDITOR mate
+set -gx VISUAL mate
 
 # Add gem path to our $PATH as a first entry for commands
 if status --is-login
@@ -43,20 +43,20 @@ set -gx LESS "-RMSX~gis"
 
 # Colorful man pages
 # from http://pastie.org/pastes/206041/text
-setenv -gx LESS_TERMCAP_mb (printf "\e[1;31m")        # begin blinking (red)
-setenv -gx LESS_TERMCAP_md (printf "\e[1;91m")        # begin bold (light red)
-setenv -gx LESS_TERMCAP_me (printf "\e[0m")           # end mode
-setenv -gx LESS_TERMCAP_se (printf "\e[0m")           # end standout-mode
-setenv -gx LESS_TERMCAP_so (printf "\e[30;47m")       # begin standout-mode - info box (grey background, black text)
-setenv -gx LESS_TERMCAP_ue (printf "\e[0m")           # end underline
-setenv -gx LESS_TERMCAP_us (printf "\e[04;36m")       # begin underline (cyan)
+set -gx LESS_TERMCAP_mb (printf "\e[1;31m")        # begin blinking (red)
+set -gx LESS_TERMCAP_md (printf "\e[1;91m")        # begin bold (light red)
+set -gx LESS_TERMCAP_so (printf "\e[30;47m")       # begin standout-mode - info box (grey background, black text)
+set -gx LESS_TERMCAP_us (printf "\e[04;36m")       # begin underline (cyan)
+set -gx LESS_TERMCAP_se (printf "\e[0m")           # end standout-mode
+set -gx LESS_TERMCAP_ue (printf "\e[0m")           # end underline
+set -gx LESS_TERMCAP_me (printf "\e[0m")           # end mode
 
 # grep colors
-setenv -gx GREP_COLOR '4;32' # green underlined
-setenv -gx GREP_OPTIONS "--color=auto"
+set -gx GREP_COLOR '4;32' # green underlined
+set -gx GREP_OPTIONS "--color=auto"
 
 # ls colors
-setenv -gx LSCOLORS 'gxfxbgaeBxxfhehbaghbad'          # see man ls
+set -gx LSCOLORS 'gxfxbgaeBxxfhehbaghbad'          # see man ls
 
 # Settings for timing helper function
 source ~/.config/fish/functions/fish_command_timer.fish
@@ -70,10 +70,3 @@ set -gx NVIM_TUI_ENABLE_CURSOR_SHAPE 1
 
 # Acces token for football stats
 set -gx SOCCER_CLI_API_TOKEN "f8e416cdeeb24e33824e96b1bcfc3961"
-
-# disable Google analytics for homebrew
-set -gx HOMEBREW_NO_ANALYTICS 1
-
-abbr tv transcode-video --mp4 --720p --target small --quick --audio-width all=stereo --add-subtitle nld
-abbr vidl youtube-dl -f pg-nettv
-abbr quad xattr -d com.apple.quarantine
