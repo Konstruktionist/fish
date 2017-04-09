1e9e0ddeede4837c308a46bf8659775b18e251b2
Konstruktionist <pvanpeursem@gmail.com>
2017-04-09 18:54:01 +0200

    Made action messages italic

--- functions/bup.fish
+++ functions/bup.fish
@@ -1,14 +1,14 @@
 function bup -d 'Brew update & upgrade'
 
-  set_color yellow;
+  set_color -i yellow;
   echo -e '\n==> Updating Brew ...'
   set_color normal;
   brew update;
-  set_color yellow;
+  set_color -i yellow;
   echo -e '\n==> Upgrading Brew ...'
   set_color normal;
   brew upgrade;
-  set_color yellow;
+  set_color -i yellow;
   echo -e '\n==> Cleaning up Brew ...'
   set_color normal;
   brew cleanup;
