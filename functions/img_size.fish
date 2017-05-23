0b6cbe60165b8084cca41998b60b5c3a10a1fb31
Konstruktionist <pvanpeursem@gmail.com>
2017-05-23 11:52:43 +0200

    Change to graphicsmagick
    Why:
    
    * imagemagick is old software with apparently lots of security problems
      I've changed therefore to graphicsmagick, which has a mich better
      reputation, and claims to be faster.
    
    This change addresses the need by:
    
    * Me

--- functions/img_size.fish
+++ functions/img_size.fish
@@ -1,9 +1,9 @@
 # A function to determine image dimensions
 #
-#     dependency: imagemagick
-#     =======================
+#     dependency: graphicsmagick
+#     ==========================
 #
 
 function img_size -d 'Show image dimensions'
-  convert $argv -print "Size: %wx%h\n" /dev/null
+  gm identify -verbose $argv | egrep Geometry
 end
