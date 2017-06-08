276a5cf04c833d987b105528ded4a23196469315
Konstruktionist <pvanpeursem@gmail.com>
2017-06-08 17:19:46 +0200

    Fix color setting

--- functions/ag.fish
+++ functions/ag.fish
@@ -1,4 +1,4 @@
 # This forces ag to use the colors I prefer
 function ag
-  command ag --color-path '93' --color-match '4;32' --color-line-number 94 $argv
+  command ag --color-path '93' --color-match '4;32' --color-line-number '94' $argv
 end
