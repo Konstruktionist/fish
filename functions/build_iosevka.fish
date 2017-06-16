ac51fb45eabb4f4d6fb02674eaa709e314455b38
Konstruktionist <pvanpeursem@gmail.com>
2017-06-16 11:47:41 +0200

    Switch to gnu make

--- functions/build_iosevka.fish
+++ functions/build_iosevka.fish
@@ -3,21 +3,22 @@ function build_iosevka -d "build custom version Iosevka font"
   # Save current working directory, so we can excecute this anywhere in the file system.
   set current_working_directory $PWD
 
-  cd ~/Downloads/Iosevka-master
+  cd ~/GitRepos/Iosevka
 
   # Upgrade to latest version of required tools
-  # brew upgrade node ttfautohint otfcc-mac64
+  # brew upgrade node ttfautohint otfcc-mac64 make
   #   I update brew every day with the bup function, so I don't need this
 
   # Install required libraries for nodejs
   npm install
   # The custom configuration
-  make custom-config design='v-asterisk-low v-underscore-low v-brace-straight v-numbersign-slanted'
+  # using (brew installed) gmake because macOS version is to old
+  gmake custom-config design='v-asterisk-low v-underscore-low v-brace-straight v-numbersign-slanted'
   # Build font
-  make custom
-  cd dist/iosevka-custom
+  gmake custom
   # We have italics in gui & terminal, so we don't need obliques
-  rm *oblique*
+  cd dist/iosevka-custom/ttf
+  rm *oblique*.ttf
 
   # Return to where we came from
   cd $current_working_directory
