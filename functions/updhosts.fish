4f58003b6fdee7ed7fafe5d03ea2b58b4c45e35d
Konstruktionist <pvanpeursem@gmail.com>
2017-11-20 19:58:41 +0100

    Add fish_cleanup_hosts.fish file functionality into updhosts
    Why:
    
    * The fish_cleanup_hosts.fish file was only called from updhosts.
      It's better to put it into a function in this file, so it is not
      dependent on anything else.
    
    This change addresses the need by:
    
    * Me

--- functions/updhosts.fish
+++ functions/updhosts.fish
@@ -6,7 +6,18 @@ function updhosts -d 'Update my /etc/hosts file'
   set current_working_directory $PWD
 
   cd ~/workspace
+  
+  function fish_cleanup_hosts -d "Helper function for cleaning up downloaded hosts file"
 
+    # files downloaded from:
+    #   http://someonewhocares.org/hosts/zero/hosts
+    #   http://winhelp2002.mvps.org/hosts.txt
+    #   https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-social/hosts
+    #   https://pgl.yoyo.org
+
+    sed 's/127.0.0.1/0.0.0.0/g;' |  grep 0.0.0.0 | sed 's/[[:space:]]*#.*$//g;' | grep -v localhost | tr ' ' '\t' | tr -s '\t' | tr -d '\015'
+  end
+  
   # Clean up my blocklist
   # This is a list which I maintain with reasons why the sites are on there. If
   # in the future things seem odd, I can review if I broke something.
