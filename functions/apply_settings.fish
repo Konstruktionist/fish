4c26e53be6fd7ce12d742a823f83688fd3e81ec9
Konstruktionist <pvanpeursem@gmail.com>
2017-11-20 19:05:43 +0100

    Tweaked colors

--- functions/apply_settings.fish
+++ functions/apply_settings.fish
@@ -3,31 +3,32 @@ function apply_settings -d 'setup fish universal variables'
 
   # setup fish universal variables
 
-  set -U fish_color_autosuggestion 74fbfd  # cyan-like
-  set -U fish_color_command 4eab31  # green
-  set -U fish_color_comment 808080  --italics
-  set -U fish_color_cwd ffd787 #yellow
-  set -U fish_color_cwd_root b91e2e
-  set -U fish_color_error d14548 --bold
-  set -U fish_color_escape af5f5f
-  set -U fish_color_history_current 87afd7
-  set -U fish_color_host 5f87af
-  set -U fish_color_match d7d7d7
   set -U fish_color_normal a1a1a1
-  set -U fish_color_operator f2797b # light red
-  set -U fish_color_param 87afff
+  set -U fish_color_command 4eab31  # green
   set -U fish_color_quote fae3a0
   set -U fish_color_redirection cb1ed1
+  set -U fish_color_end f3b26d   # orange
+  set -U fish_color_error d14548 --bold
+  set -U fish_color_param 87afff
+  set -U fish_color_comment 808080  --italics
+  set -U fish_color_match d7d7d7
   set -U fish_color_search_match --background=cccccc  # gray background
+  set -U fish_color_operator f2797b # light red
+  set -U fish_color_escape af5f5f
+  set -U fish_color_cwd ffd787 #yellow
+  set -U fish_color_autosuggestion 74fbfd  # cyan-like
+  set -U fish_color_cwd_root b91e2e
+  set -U fish_color_history_current 87afd7
+  set -U fish_color_host 5f87af
   set -U fish_color_status b91e2e
   set -U fish_color_user 5f875f
   set -U fish_color_valid_path --underline
-  set -U fish_color_end f3b26d   # orange
 
-  set -U fish_pager_color_completion 75a304 #green
-  set -U fish_pager_color_description 808080 --italics # gray
-  set -U fish_pager_color_prefix 87afff # blue
-  set -U fish_pager_color_progress cyan
+  set -U fish_pager_color_prefix 87afff # blue - the color of the prefix string, i.e. the string that is to be completed
+  set -U fish_pager_color_completion 5fafff # bright blue - the color of the completion itself
+  set -U fish_pager_color_description 808080 --italics # gray - the color of the completion description
+  set -U fish_pager_color_progress 262626 --background=b2b2b2 # the color of the progress bar at the bottom left corner
+  set -U fish_pager_color_secondary d7d700 # the background color of the every second completion
 
   # Custom colors used for prompt
 
