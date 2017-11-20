167b3b3c6c27f5a5f0c04058d256871ddea352ea
Konstruktionist <pvanpeursem@gmail.com>
2017-11-20 20:04:25 +0100

    fix description and comment

--- functions/plainvim.fish
+++ functions/plainvim.fish
@@ -1,3 +1,4 @@
-function plainvim --description 'Start vim without plugins'
+function plainvim --description 'Start vim without vimrc in nocompatible mode'
+  # See :h -u and :h -N for an explanation
 	vim -u NONE -N
 end
