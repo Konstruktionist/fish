10939bbd5284d7c3e6c0e61da691816270e0e53b
Konstruktionist <pvanpeursem@gmail.com>
2017-12-03 22:57:32 +0100

    Add dependencies to comments

--- functions/npodl.fish
+++ functions/npodl.fish
@@ -1,4 +1,9 @@
 function npodl -d "download a file from the npo website"
+
+  # Dependencies: youtube-dl
+  # ========================
+  # installed via homebrew
+
   # get the list of all the the available formats and detect the highest quality
   set format (youtube-dl -F $argv | grep -i '42C033@' | grep -i 'mp4a.40.2@192k' | awk '{print $1}')
 
--- functions/vidl.fish
+++ functions/vidl.fish
@@ -1,4 +1,9 @@
 function vidl -d 'Download video with predefined settings'
+
+  # Dependencies: youtube-dl
+  # ========================
+  # installed via homebrew
+
   youtube-dl -f pg-nettv $argv
 end
   
