c9bbbe08e488212c9bc93ee99e5a98d9d1e67e9a
Konstruktionist <pvanpeursem@gmail.com>
2017-12-09 21:12:27 +0100

    Fix URL

--- functions/down4me.fish
+++ functions/down4me.fish
@@ -1,3 +1,3 @@
 function down4me -d 'Checks whether a website is down for you, or everybody'
-  curl --silent "http://www.isup.me/$argv" | grep -e "It's.*[.!]" | sed "s/\s*\(It's[^<>]*[.!]\).*\$/\1/"
+  curl --silent "http://downforeveryoneorjustme.com/$argv" | grep -e "It's.*[.!]" | sed "s/\s*\(It's[^<>]*[.!]\).*\$/\1/"
 end
