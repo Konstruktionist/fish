2a880cfbba21a022aa3d0c0dd9a07acba12931fb
Konstruktionist <pvanpeursem@gmail.com>
2018-01-07 21:16:31 +0100

    Add comments about the flags

--- functions/plainvim.fish
+++ functions/plainvim.fish
@@ -1,4 +1,6 @@
 function plainvim --description 'Start vim without vimrc in nocompatible mode'
-  # See :h -u and :h -N for an explanation
+  # -u NONE prevents vim from loading vimrc
+  # -U NONE prevents vim from loading gvimrc
+  # -N tells vim to use no-compatible mode
 	vim -u NONE -N
 end
