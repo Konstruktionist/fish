42cb9b4623b2407e0a538835a217d042a2088075
Konstruktionist <pvanpeursem@gmail.com>
2018-06-26 10:17:21 +0200

    Fix man-page searching
    Why:
    
    * Thanks to stackexchange I was made aware that on BSD-like systems
      some searches didn't work. Man pages contain nroff generated control
      characters in the pages headings (and maybe elsewhere too).
      Piping the man page through `col` first and then grepping fixes this.
      See:
      https://apple.stackexchange.com/questions/328703/
    
      The accepted answer there doesn't work for me, but the col trick does.
    
    This change addresses the need by:
    
    * Me

--- functions/mans.fish
+++ functions/mans.fish
@@ -4,7 +4,12 @@
 #           surrounding each hit.
 #             Example: mans mplayer codec
 #   ---------------------------------------------------------------------------
+#
+#   piping the man page trough col is neccesary to strip nroff control
+#   characters. The -b option strips backspace chars.
+#   Thanks to:
+#   https://apple.stackexchange.com/questions/328703/
 
 function mans -d 'Search manpage given in argument 1 for term given in argument 2'
-  man $argv[1] | grep -iC7 --color=always $argv[2] | less -RX
+  man $argv[1] | col -b | grep -iC7 --color=always $argv[2] | less -RX
 end
