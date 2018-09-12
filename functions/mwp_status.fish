2294bad9c29487161dd5add94e3347b92c00fca5
Konstruktionist <pvanpeursem@gmail.com>
2018-09-12 16:27:57 +0200

    Refactor mwp_status
    Why:
    
    * Removed calls to create a user friendly date and put it inside a
      function. Made it it semi-private function by prefixing it with __k_
      This keeps it hidden from `functions` in fish, but does show up if I
      use `functions -a`.
    
      Also made SIP status response with colors.
      green == enabled, red == disabled
    
    This change addresses the need by:
    
    * Me

--- functions/mwp_status.fish
+++ functions/mwp_status.fish
@@ -6,7 +6,9 @@ function mwp_status -d "Check Apple's malware protection status"
 
   # Re-usable colors
   set normal (set_color normal)
-  set decor (set_color 87afff) # blueish purple
+  set version_color (set_color 87afff) # blueish purple
+  set green (set_color 76c376)
+  set red (set_color ff8787)
 
   # Files & their Paths:
   set -l XProtect '/System/Library/CoreServices/XProtect.bundle/Contents/version.plist'
@@ -19,6 +21,31 @@ function mwp_status -d "Check Apple's malware protection status"
 
   set -l fileName XProtect Gatekeeper SIP MRT CoreSuggest IncompatibleKernelExt CoreLSDK
 
+  function __k_userFriendlyDatum  # convert result of stat to a human readable date
+    #   NOTE: leading underscores keeps this hidden from `functions` not
+    #         `functions -a`.
+    #         __k_ prefix is used for my hidden semi-private functions.
+    #
+    #   Put everything on it's own line with
+    #     tr '\ ' '\n'
+    #   stat gives 4 timestamps in this order: st_atime, st_mtime, st_ctime, st_birthtime
+    #      st_atime = last access time in seconds since the epoch
+    #      st_mtime = last modify time in seconds since the epoch
+    #      st_ctime = inode change time (NOT creation time!) in seconds since the epoch
+    #      st_birthtime = time when the inode was created in seconds since the epoch
+    #   The timestamp we need is st_ctime, it is the time that the file was changed on OUR disk,
+    #     the other timestamps represent the time that Apple modified the file
+    #   Search for the inode changed time with
+    #     egrep -i 'st_ctime'
+    #   Let cut grab the value we are interested in
+    #     cut -d = -f 2
+    #   This is the epoch value (i.e. time since unix began)
+    #   Transform this into a human readable format taking into account locale setting (-l)
+    #     strptime -l -i "%s" -f "%d %b %Y %H:%M"
+
+    tr '\ ' '\n' | egrep -i 'st_ctime' | cut -d = -f 2 | strptime -l -i "%s" -f "%d %b %Y %H:%M"
+  end
+
   # Get the versions of the files & display dates in an understandable way
   echo "Name                    Version  Updated"
   echo "--------------------------------------------------"
@@ -28,54 +55,49 @@ function mwp_status -d "Check Apple's malware protection status"
         set version (awk -F[<>] '/CFBundleShortVersionString/ { getline; print $3}' $XProtect)
 
         #   How the 'datum' part works:
-        #   The stat utility displays information about the file pointed to by 'file'.
+        #   The stat utility displays info about the file pointed to by '$file'.
         #   Useful flag is -s. man page says:
         #     -s  Display information in 'shell output', suitable for initializing variables.
-        #   Put everything on it's own line with
-        #     tr '\ ' '\n'
-        #   stat gives 4 timestamps in this order: st_atime, st_mtime, st_ctime, st_birthtime
-        #      st_atime = last access time in seconds since the epoch
-        #      st_mtime = last modify time in seconds since the epoch
-        #      st_ctime = inode change time (NOT creation time!) in seconds since the epoch
-        #      st_birthtime = time when the inode was created in seconds since the epoch
-        #   The timestamp we need is st_ctime, it is the time that the file was changed on OUR disk,
-        #     the other timestamps represent the time that Apple modified the file
-        #   Search for the inode changed time with
-        #     egrep -i 'st_ctime'
-        #   Let cut grab the value we are interested in
-        #     cut -d = -f 2
-        #   This is the epoch value (i.e. time since unix began)
-        #   Transform this into a human readable format taking into account locale setting (-l)
-        #     strptime -l -i "%s" -f "%d %b %Y %H:%M"
+        #   Pass it to __k_userFriendlyDatum function
 
-        set datum (stat -s  $XProtect | tr '\ ' '\n' | egrep -i 'st_ctime' | cut -d = -f 2 | strptime -l -i "%s" -f "%d %b %Y %H:%M")
-        echo "$file               $decor $version $normal    $datum"
+        set datum (stat -s  $XProtect | __k_userFriendlyDatum)
+        echo "$file               $version_color $version $normal    $datum"
       case Gatekeeper
         set version (awk -F[<>] '/CFBundleShortVersionString/ { getline; print $3}' $Gatekeeper)
-        set datum (stat -s  $Gatekeeper | tr '\ ' '\n' | egrep -i 'st_ctime' | cut -d = -f 2 | strptime -l -i "%s" -f "%d %b %Y %H:%M")
-        echo "$file             $decor $version $normal     $datum"
+        set datum (stat -s  $Gatekeeper | __k_userFriendlyDatum)
+        echo "$file             $version_color $version $normal     $datum"
       case SIP
         set version (awk -F[<>] '/CFBundleShortVersionString/ { getline; print $3}' $SIP)
-        set datum (stat -s  $SIP | tr '\ ' '\n' | egrep -i 'st_ctime' | cut -d = -f 2 | strptime -l -i "%s" -f "%d %b %Y %H:%M")
-        echo "$file                    $decor $version $normal    $datum"
+        set datum (stat -s  $SIP | __k_userFriendlyDatum)
+        echo "$file                    $version_color $version $normal    $datum"
       case MRT
         set version (awk -F[<>] '/CFBundleShortVersionString/ { getline; print $3}' $MRT)
-        set datum (stat -s  $MRT | tr '\ ' '\n' | egrep -i 'st_ctime' | cut -d = -f 2 | strptime -l -i "%s" -f "%d %b %Y %H:%M")
-        echo "$file                    $decor $version $normal    $datum"
+        set datum (stat -s  $MRT | __k_userFriendlyDatum)
+        echo "$file                    $version_color $version $normal    $datum"
       case CoreSuggest
         set version (awk -F[<>] '/CFBundleShortVersionString/ { getline; print $3}' $CoreSuggest)
-        set datum (stat -s  $CoreSuggest | tr '\ ' '\n' | egrep -i 'st_ctime' | cut -d = -f 2 | strptime -l -i "%s" -f "%d %b %Y %H:%M")
-        echo "$file            $decor $version $normal    $datum"
+        set datum (stat -s  $CoreSuggest | __k_userFriendlyDatum)
+        echo "$file            $version_color $version $normal    $datum"
       case IncompatibleKernelExt
         set version (awk -F[<>] '/CFBundleShortVersionString/ { getline; print $3}' $IncompatibleKernelExt)
-        set datum (stat -s  $IncompatibleKernelExt | tr '\ ' '\n' | egrep -i 'st_ctime' | cut -d = -f 2 | strptime -l -i "%s" -f "%d %b %Y %H:%M")
-        echo "$file  $decor $version $normal  $datum"
+        set datum (stat -s  $IncompatibleKernelExt | __k_userFriendlyDatum)
+        echo "$file  $version_color $version $normal  $datum"
       case CoreLSDK
         set version (awk -F[<>] '/CFBundleShortVersionString/ { getline; print $3}' $CoreLSDK)
-        set datum (stat -s  $CoreLSDK | tr '\ ' '\n' | egrep -i 'st_ctime' | cut -d = -f 2 | strptime -l -i "%s" -f "%d %b %Y %H:%M")
-        echo "$file               $decor $version $normal       $datum"
+        set datum (stat -s  $CoreLSDK | __k_userFriendlyDatum)
+        echo "$file               $version_color $version $normal       $datum"
     end
   end
-  echo '---'
-  csrutil status
+
+  echo '-----'
+  set SIP_status (csrutil status | awk '{print $5}')
+  switch $SIP_status
+    case enabled.
+      echo "System Integrity Protection status:$green enabled $normal"
+    case disabled.
+      echo "System Integrity Protection status:$red disabled $normal"
+    case '*'
+      echo "Unknown"
+  end
+
 end
