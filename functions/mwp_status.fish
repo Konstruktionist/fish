6f0c82d625ef17165f17f3d8712aac99d4a35400
Konstruktionist <pvanpeursem@gmail.com>
2018-09-24 20:32:25 +0200

    Change flow of functions
    Why:
    
    * Better readability of code for me
    
    This change addresses the need by:
    
    * Me

--- functions/mwp_status.fish
+++ functions/mwp_status.fish
@@ -21,31 +21,6 @@ function mwp_status -d "Check Apple's malware protection status"
 
   set -l fileName XProtect Gatekeeper SIP MRT CoreSuggest IncompatibleKernelExt CoreLSDK
 
-  function __k_userFriendlyDatum  # convert result of stat to a human readable date
-    #   NOTE: leading underscores keeps this hidden from `functions` not
-    #         `functions -a`.
-    #         __k_ prefix is used for my hidden semi-private functions.
-    #
-    #   Put everything on it's own line with
-    #     tr '\ ' '\n'
-    #   stat gives 4 timestamps in this order: st_atime, st_mtime, st_ctime, st_birthtime
-    #      st_atime = last access time in seconds since the epoch
-    #      st_mtime = last modify time in seconds since the epoch
-    #      st_ctime = inode change time (NOT creation time!) in seconds since the epoch
-    #      st_birthtime = time when the inode was created in seconds since the epoch
-    #   The timestamp we need is st_ctime, it is the time that the file was changed on OUR disk,
-    #     the other timestamps represent the time that Apple modified the file
-    #   Search for the inode changed time with
-    #     egrep -i 'st_ctime'
-    #   Let cut grab the value we are interested in
-    #     cut -d = -f 2
-    #   This is the epoch value (i.e. time since unix began)
-    #   Transform this into a human readable format taking into account locale setting (-l)
-    #     strptime -l -i "%s" -f "%d %b %Y %H:%M"
-
-    tr '\ ' '\n' | egrep -i 'st_ctime' | cut -d = -f 2 | strptime -l -i "%s" -f "%d %b %Y %H:%M"
-  end
-
   # Get the versions of the files & display dates in an understandable way
   echo "Name                    Version  Updated"
   echo "--------------------------------------------------"
@@ -100,4 +75,30 @@ function mwp_status -d "Check Apple's malware protection status"
       echo "Unknown"
   end
 
+end
+
+# convert result of stat to a human readable date
+function __k_userFriendlyDatum
+  #       NOTE: leading underscores keeps this hidden from `functions` not
+  #             `functions -a`.
+  #             __k_ prefix is used for my hidden semi-private functions.
+  #
+  #   Put everything on it's own line with
+  #     tr '\ ' '\n'
+  #   stat gives 4 timestamps in this order: st_atime, st_mtime, st_ctime, st_birthtime
+  #      st_atime = last access time in seconds since the epoch
+  #      st_mtime = last modify time in seconds since the epoch
+  #      st_ctime = inode change time (NOT creation time!) in seconds since the epoch
+  #      st_birthtime = time when the inode was created in seconds since the epoch
+  #   The timestamp we need is st_ctime, it is the time that the file was changed on OUR disk,
+  #     the other timestamps represent the time that Apple modified the file
+  #   Search for the inode changed time with
+  #     egrep -i 'st_ctime'
+  #   Let cut grab the value we are interested in
+  #     cut -d = -f 2
+  #   This is the epoch value (i.e. time since unix began)
+  #   Transform this into a human readable format taking into account locale setting (-l)
+  #     strptime -l -i "%s" -f "%d %b %Y %H:%M"
+
+  tr '\ ' '\n' | egrep -i 'st_ctime' | cut -d = -f 2 | strptime -l -i "%s" -f "%d %b %Y %H:%M"
 end
