66091691b5119076c2b190b47f275003ce5e4127
Konstruktionist <pvanpeursem@gmail.com>
2018-10-27 13:11:38 +0200

    Upgraded to https

--- functions/down4me.fish
+++ functions/down4me.fish
@@ -1,3 +1,3 @@
 function down4me -d 'Checks whether a website is down for you, or everybody'
-  curl --silent "http://downforeveryoneorjustme.com/$argv" | grep -e "It's.*[.!]" | sed "s/\s*\(It's[^<>]*[.!]\).*\$/\1/"
+  curl --silent "https://downforeveryoneorjustme.com/$argv" | grep -e "It's.*[.!]" | sed "s/\s*\(It's[^<>]*[.!]\).*\$/\1/"
 end
