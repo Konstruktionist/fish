33bab891535bb663e2e9f95b506e125cd86760b2
Konstruktionist <pvanpeursem@gmail.com>
2018-12-20 21:33:41 +0100

    Fix pathname for looooong paths & integrate ssh colors
    Why:
    
    * If we were in a very deep directory structure, fish will fallback to a
      very simple prompt. This isn't very helpful, only the first letter of
      each directory is shown and a > prompt. Also if we were in a git repo
      the git info in the prompt was lost.
      Now we retain all this info and the dir names are reduced to what is
      set in the fish_prompt_pwd_dir_length variable in config.fish.
    
    This change addresses the need by:
    
    * Me

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -4,17 +4,32 @@ function fish_prompt -d 'Write out the prompt'
   #
   #   1. display username & host when in a ssh session
   #   2. current directory
-  #   3. 1. git branch name (if we are in a git repo)
-  #      2. wether or not the working copy is dirty
-  #      3. git sha hash
-  #      4. display up/down arrow if there is stuff to be pushed/pulled (this
-  #           will probably go into the prompt_git_status function)
+  #       shortened to 'x' chars per directory if path length is > 60 chars long
+  #         where 'x' is defined by fish_prompt_pwd_dir_length (default = 1)
+  #         This can be changed in config.fish
+  #       otherwise show full path
+  #   3. git info:  (from prompt_git_status) 
+  #      a. git branch name (if we are in a git repo)
+  #      b. wether or not the working copy is dirty
+  #      c. git sha hash
+  #      d. display up/down arrow if there is stuff to be pushed/pulled
   #   4. display prompt symbol (in red if last command exited with an error)
 
-  set last_status $status
+  set -l last_status $status
+  set -l ssh_root_color ff8787
+  set -l ssh_user_color 6bb9f0
 
   # Get the current working directory & simplify $HOME to ~
-  set -l cur_wd (echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||')
+  set cur_wd (echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||')
+  # Do we have to shorten the string?
+  if test (string length $cur_wd) -gt 60
+    set -q fish_prompt_pwd_dir_length
+    or set -l fish_prompt_pwd_dir_length 1
+    set cur_wd (prompt_pwd)
+  else
+    set cur_wd $cur_wd
+    # set cur_wd (echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||')
+  end
 
   # 1. ssh session
   if test -n "$SSH_CONNECTION"
@@ -23,9 +38,9 @@ function fish_prompt -d 'Write out the prompt'
 
     # color of user@host is set by $user, either red (root) or blue (non-root)
     if test "$user" = "root"
-      set user (set_color -r $fish_color_error)"$user"
+      set user (set_color -r $ssh_root_color)"$user"
     else
-      set user (set_color -r 6bb9f0)"$user"
+      set user (set_color -r $ssh_user_color)"$user"
     end
 
     set user_host "$user@$host"; echo -n $user_host; set_color normal ; echo -n " "
@@ -42,7 +57,7 @@ function fish_prompt -d 'Write out the prompt'
   if test "$last_status" -ne 0
     set_color $fish_color_error # Red
   end
-   echo -n '▶︎ '
+  echo -n '▶︎ '
   set_color normal
 
 end
