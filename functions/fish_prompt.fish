b8b14219bcd898d1a012d5d3c058383fcf864ec4
Konstruktionist <pvanpeursem@gmail.com>
2018-12-21 17:05:42 +0100

    Add test for logged in as root on local machine
    Why:
    
    * Although it is unlikely on a Mac to have a root user account it's
    possible. So lets check for that and display root in reversed red before
    the pathname. If we are a normal user (admin or not) we show no
    username.
    Also if we ever jump to Linux (where some distro's do have a root user)
    we will be prepared.
    
    This change addresses the need by:
    
    * Me

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -3,21 +3,23 @@ function fish_prompt -d 'Write out the prompt'
   # What the prompt has to show
   #
   #   1. display username & host when in a ssh session
-  #   2. current directory
-  #       shortened to 'x' chars per directory if path length is > 60 chars long
-  #         where 'x' is defined by fish_prompt_pwd_dir_length (default = 1)
-  #         This can be changed in config.fish
-  #       otherwise show full path
-  #   3. git info:  (from prompt_git_status) 
+  #   2. username if we're logged in as root
+  #   3. current directory
+  #      a. shortened to 'x' chars per directory if path length is > 60 chars long
+  #           where 'x' is defined by fish_prompt_pwd_dir_length (default = 1)
+  #           This can be changed in config.fish
+  #      b. otherwise show full path
+  #   4. git info:  (from prompt_git_status) 
   #      a. git branch name (if we are in a git repo)
   #      b. wether or not the working copy is dirty
   #      c. git sha hash
   #      d. display up/down arrow if there is stuff to be pushed/pulled
-  #   4. display prompt symbol (in red if last command exited with an error)
+  #   5. display prompt symbol (in red if last command exited with an error)
 
   set -l last_status $status
   set -l ssh_root_color ff8787
   set -l ssh_user_color 6bb9f0
+  set -l user (whoami)
 
   # Get the current working directory & simplify $HOME to ~
   set cur_wd (echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||')
@@ -28,13 +30,11 @@ function fish_prompt -d 'Write out the prompt'
     set cur_wd (prompt_pwd)
   else
     set cur_wd $cur_wd
-    # set cur_wd (echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||')
   end
 
   # 1. ssh session
   if test -n "$SSH_CONNECTION"
     set -l host (hostname -s)
-    set -l user (whoami)
 
     # color of user@host is set by $user, either red (root) or blue (non-root)
     if test "$user" = "root"
@@ -46,14 +46,23 @@ function fish_prompt -d 'Write out the prompt'
     set user_host "$user@$host"; echo -n $user_host; set_color normal ; echo -n " "
   end
 
-  # 2. current directory
+  # 2. Are we logged in as root?
+  if test "$user" = "root"
+    set user (set_color -r red)"$user"
+    echo -n $user; set_color normal; echo -n ' '
+  else
+    set user ''
+  echo -n $user
+  end
+
+  # 3. current directory
   set_color $fish_color_cwd; echo -n $cur_wd; set_color normal
 
-  # 3. Git info
+  # 4. Git info
   prompt_git_status
   echo    # effectivly sets a newline (\n)
 
-  # 4. Prompt symbol
+  # 5. Prompt symbol
   if test "$last_status" -ne 0
     set_color $fish_color_error # Red
   end
