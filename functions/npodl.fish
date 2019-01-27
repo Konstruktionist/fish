08444622653aa33447a1170b1fbed8e292da4f57
Konstruktionist <pvanpeursem@gmail.com>
2019-01-27 21:04:45 +0100

    Make format selection more robust
    Why:
    
    * Not specifying the exact bitrate but searching for the best quality
    makes this more robust. They can change the bitrates anytime they want,
    we will just look for the last entry in the list that has a specific
    format specifier, and select that. This should keep working as long as
    they keep the list in the order it is now, i.e. from lowest to highest
    quality.
    
    This change addresses the need by:
    
    * Me

--- functions/npodl.fish
+++ functions/npodl.fish
@@ -1,13 +1,13 @@
-function npodl -d "download a file from the npo website"
+function npodl -d "download a video from the npo website"
 
   # Dependencies: youtube-dl
   # ========================
   # installed via homebrew
 
   # get the list of all the the available formats and detect the highest quality
-  set format (youtube-dl -F $argv | grep -i '42C033@' | grep -i 'mp4a.40.2@192k' | awk '{print $1}')
+  set format (youtube-dl -F $argv | grep -i '42C033@' | awk '{print $1}' | xargs | string sub --start=-8)
 
   # download this file
   youtube-dl -f $format $argv
 end
-  
+
