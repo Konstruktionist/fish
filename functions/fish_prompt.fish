4d833f082403947d5e1eb41f63eeab7763111c1f
Konstruktionist <pvanpeursem@gmail.com>
2019-02-10 15:07:53 +0100

    Fix prompt: show user@host when local root & ssh
    Why:
    
    * sudo -s is the problem: there is no distinction in environment if we
    do a sudo -s locally or over a ssh connection. So when I did a sudo -s
    over an ssh connection the prompt only showed 'root' in reversed red,
    giving the impression that we are on the local machine. Now we show the
    user@host whether we are on a local machine or on a ssh connection.
    
    It is a bit ugly, as I like my prompt to be as minimal as possible. So I
    will try to find a way to only show 'root' if we are on a local machine
    and 'user@host' if we are on a ssh connection. For the moment this will
    have to do.
    
    This change addresses the need by:
    
    * Me

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,22 +2,24 @@ function fish_prompt -d 'Write out the prompt'
 
   # What the prompt has to show
   #
-  #   1. display username & host when in a ssh session
-  #   2. username if we're logged in as root on localhost
-  #   3. current directory
+  #   1. display username & host when in a ssh session or sudo -s locally
+  #   2. current directory
   #      a. shortened to 'x' chars per directory if path length is > 60 chars long
   #           where 'x' is defined by fish_prompt_pwd_dir_length (default = 1)
   #           This can be changed in config.fish
   #      b. otherwise show full path
-  #   4. git info:  (from prompt_git_status) 
+  #   3. git info:  (from prompt_git_status)
   #      a. git branch name (if we are in a git repo)
   #      b. wether or not the working copy is dirty
   #      c. git sha hash
   #      d. display up/down arrow if there is stuff to be pushed/pulled
-  #   5. display prompt symbol (in red if last command exited with an error)
+  #   4. display prompt symbol 
+  #      in red if last command exited with an error + leading error code in []
 
   set -l last_status $status
   set -l user (whoami)
+  set -l host (hostname -s)
+  set -l user_host "$user@$host"
 
   # Get the current working directory & simplify $HOME to ~
   set cur_wd (echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||')
@@ -30,45 +32,48 @@ function fish_prompt -d 'Write out the prompt'
     set cur_wd $cur_wd
   end
 
-  # 1. ssh session
-  if test -n "$SSH_CONNECTION"
-    set -l host (hostname -s)
+  # 1. display username & host when in a ssh session or sudo -s locally
+  # Ideally we show no user@host info when we are on the local machine even
+  # when we are root. I would like to show just root on local machine after
+  # we do sudo -s. However if we do  'sudo -s'  either local or over ssh we
+  # have no information in our environment (env) to distinguish between the
+  # two. So we show user@host when we are root & as a non-root user over ssh
+  # and after sudo -s locally.
+  # color of user@host is set by $user, either red (root) or blue (non-root)
 
-    # color of user@host is set by $user, either red (root) or blue (non-root)
-    if test "$user" = "root"
-      set user (set_color -r red)"$user"
-    else
-      set user (set_color -r blue)"$user"
-    end
-
-    set user_host "$user@$host"
-    echo -n $user_host; set_color normal ; echo -n ' '
+  switch $user
+    case root
+      set_color -r red
+      echo -n $user_host
+      set_color normal
+      echo -n ' '
+    case '*'              # non-root
+      # As non-root we can test if we have a SSH_TTY in our environment. If we
+      # do we have an ssh-connection, and show a blue user@host info.
+      # If we are on the local machine as a normal user we don't show user@host.
+      if test -n "$SSH_TTY"
+        set_color -r blue
+        echo -n $user_host
+        set_color normal
+        echo -n ' '
+      else
+        echo -n ''
+      end
   end
 
-  # 2. Are we logged in as root on localhost?
-  if test -z "$SSH_CONNECTION"
-    if test "$user" = "root"
-      set user (set_color -r red)"$user"
-      echo -n $user; set_color normal; echo -n ' '
-    else
-      set user ''
-      echo -n $user
-    end
-  end
-
-  # 3. current directory
+  # 2. current directory
   set_color $fish_color_cwd; echo -n $cur_wd; set_color normal
 
-  # 4. Git info
+  # 3. Git info
   prompt_git_status
   echo    # effectivly sets a newline (\n)
 
-  # 5. Prompt symbol
+  # 4. Prompt symbol
   if test "$last_status" -ne 0
     set_color $fish_color_error # Red
+    echo -n "[$last_status]"
   end
   # prompt indicator = BLACK CIRCLE (U+25CF) followed by INTERROBANG (U+203D) and a space
   set_color -o; echo -n '●‽ '
   set_color normal
-
 end
