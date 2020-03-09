65f83ea1bb2d9feb4123d8bf5e1a5675d9b62ac4
Konstruktionist <pvanpeursem@gmail.com>
2020-03-09 12:45:27 +0100

    Remove gitk.fish
    git version 2.25.1 brew formula stated:
    The Tcl/Tk GUIs (e.g. gitk, git-gui) are now in the `git-gui` formula.
    So I don't need this to prevent gitk from starting

--- functions/gitk.fish
+++
@@ -1,5 +0,0 @@
-function gitk -d "prevent the horrible X11 app gitk to show up"
-  # It's nice that they tried, but there are better alternatives
-   command -sq gitk 2>/dev/null
-   echo "gitk is awfull. Use a better application"
-end
